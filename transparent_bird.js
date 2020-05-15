11bde77906e546d0ec1839f162ab0a28275c11c6
atarng@github.com <atarng@github.com>
2020-05-15 10:08:59 -0400

    transparent bird test

--- transparent_bird.js
+++ transparent_bird.js
@@ -1,7 +1,5 @@
 ////////////////////////////////////////////////////////////////////////////////
-// Make hat, body, leg and foot of Drinking Bird shiny
-////////////////////////////////////////////////////////////////////////////////
-/*global THREE, Coordinates, document, window */
+/*global THREE, Coordinates, document, window  */
 var camera, scene, renderer;
 var cameraControls;
 
@@ -15,129 +13,153 @@ function fillScene() {
 
 	scene.add( new THREE.AmbientLight( 0x222222 ) );
 
-	var light = new THREE.DirectionalLight( 0xFFFFFF, 0.7 );
+	var light = new THREE.DirectionalLight( 0xffffff, 0.7 );
 	light.position.set( 200, 500, 500 );
 
 	scene.add( light );
-
-	light = new THREE.DirectionalLight( 0xFFFFFF, 0.9 );
+	
+	light = new THREE.DirectionalLight( 0xffffff, 0.9 );
 	light.position.set( -200, -100, -400 );
 
 	scene.add( light );
-
+	
+	//  DEPRECATED
 	// Coordinates.drawGround({size:1000});
+	drawGrid(10,10);
 
-	createDrinkingBird();
+	drawDrinkingBird();
 }
 
-function createDrinkingBird() {
+function drawDrinkingBird() {
+
 	//////////////////////////////
-	// YOU SHOULD MODIFY THE SHININESS:
-	// use MeshPhongMaterial
-	// hat and body: 100
-	// leg: 4
-	// foot: 30
-	// specular color for all of these to 0.5,0.5,0.5
+	// MATERIALS
 	var headMaterial = new THREE.MeshLambertMaterial( );
 	headMaterial.color.r = 104/255;
 	headMaterial.color.g = 1/255;
 	headMaterial.color.b = 5/255;
-
+				
 	var hatMaterial = new THREE.MeshPhongMaterial( { shininess: 100 } );
 	hatMaterial.color.r = 24/255;
 	hatMaterial.color.g = 38/255;
 	hatMaterial.color.b = 77/255;
 	hatMaterial.specular.setRGB( 0.5, 0.5, 0.5 );
-
-	// new THREE.MeshLambertMaterial( );
+				
 	var bodyMaterial = new THREE.MeshPhongMaterial( { shininess: 100 } );
 	bodyMaterial.color.setRGB( 31/255, 86/255, 169/255 );
 	bodyMaterial.specular.setRGB( 0.5, 0.5, 0.5 );
 
-	//new THREE.MeshLambertMaterial( );
+	// MODIFY THIS TO BE TRANSPARENT:
+	var glassMaterial = new THREE.MeshPhongMaterial( {
+		color: 0x0
+	, specular: 0xFFFFFF
+	, shininess: 100
+	, opacity: 0.3
+	, transparent: true 
+	} );
+	
 	var legMaterial = new THREE.MeshPhongMaterial( { shininess: 4 } );
 	legMaterial.color.setHex( 0xAdA79b );
-	legMaterial.specular.setRGB( 0.5, 0.5, 0.5 ); 
-
-	var footMaterial = new THREE.MeshPhongMaterial({
-		color: 0x960f0b
-	, shininess: 30
-	} );
+	legMaterial.specular.setRGB( 0.5, 0.5, 0.5 );
+				
+	var footMaterial = new THREE.MeshPhongMaterial( { color: 0x960f0b, shininess: 30 } );
 	footMaterial.specular.setRGB( 0.5, 0.5, 0.5 );
-
+				
 	var sphere, cylinder, cube;
-
+	
 	var bevelRadius = 1.9;	// TODO: 2.0 causes some geometry bug.
 
 	// MODELS
 	// base
-	// DEPRECATED: THREE.BeveledBlockGeometry
-	// cube = new THREE.Mesh(new THREE.BeveledBlockGeometry( 20+64+110, 4, 2*77+12, bevelRadius ), footMaterial );
-	cube = new THREE.Mesh(new THREE.BoxGeometry( 20+64+110, 4, 2*77+12), footMaterial );
+	cube = new THREE.Mesh( 
+		new THREE.BoxGeometry( 20+64+110, 4, 2*77+12 ), footMaterial );
 	cube.position.x = -45;	// (20+32) - half of width (20+64+110)/2
 	cube.position.y = 4/2;	// half of height
 	cube.position.z = 0;	// centered at origin
 	scene.add( cube );
-
+				
 	// feet
-	cube = new THREE.Mesh(
+	cube = new THREE.Mesh( 
 		new THREE.BoxGeometry( 20+64+110, 52, 6 ), footMaterial );
 	cube.position.x = -45;	// (20+32) - half of width (20+64+110)/2
 	cube.position.y = 52/2;	// half of height
 	cube.position.z = 77 + 6/2;	// offset 77 + half of depth 6/2
 	scene.add( cube );
-
-	cube = new THREE.Mesh(
+				
+	cube = new THREE.Mesh( 
 		new THREE.BoxGeometry( 20+64+110, 52, 6 ), footMaterial );
 	cube.position.x = -45;	// (20+32) - half of width (20+64+110)/2
 	cube.position.y = 52/2;	// half of height
 	cube.position.z = -(77 + 6/2);	// negative offset 77 + half of depth 6/2
 	scene.add( cube );
-
-	cube = new THREE.Mesh(
+				
+	cube = new THREE.Mesh( 
 		new THREE.BoxGeometry( 64, 104, 6 ), footMaterial );
 	cube.position.x = 0;	// centered on origin along X
 	cube.position.y = 104/2;
 	cube.position.z = 77 + 6/2;	// negative offset 77 + half of depth 6/2
 	scene.add( cube );
-
-	cube = new THREE.Mesh(
+				
+	cube = new THREE.Mesh( 
 		new THREE.BoxGeometry( 64, 104, 6 ), footMaterial );
 	cube.position.x = 0;	// centered on origin along X
 	cube.position.y = 104/2;
 	cube.position.z = -(77 + 6/2);	// negative offset 77 + half of depth 6/2
 	scene.add( cube );
-
+				
 	// legs
-	cube = new THREE.Mesh(
+	cube = new THREE.Mesh( 
 		new THREE.BoxGeometry( 60, 282+4, 4 ), legMaterial );
 	cube.position.x = 0;	// centered on origin along X
 	cube.position.y = 104 + 282/2 - 2;
 	cube.position.z = 77 + 6/2;	// negative offset 77 + half of depth 6/2
 	scene.add( cube );
-
-	cube = new THREE.Mesh(
+				
+	cube = new THREE.Mesh( 
 		new THREE.BoxGeometry( 60, 282+4, 4 ), legMaterial );
 	cube.position.x = 0;	// centered on origin along X
 	cube.position.y = 104 + 282/2 - 2;
 	cube.position.z = -(77 + 6/2);	// negative offset 77 + half of depth 6/2
 	scene.add( cube );
-
+				
 	// body
 	sphere = new THREE.Mesh(
-		new THREE.SphereGeometry( 116/2, 32, 16 ), bodyMaterial );
+		new THREE.SphereGeometry( 104/2, 32, 16, 0, Math.PI * 2, Math.PI/2, Math.PI ), bodyMaterial );
+	sphere.position.x = 0;
+	sphere.position.y = 160;
+	sphere.position.z = 0;
+	scene.add( sphere );
+	
+	// cap for top of hemisphere
+	cylinder = new THREE.Mesh( 
+		new THREE.CylinderGeometry( 104/2, 104/2, 0, 32 ), bodyMaterial );
+	cylinder.position.x = 0;
+	cylinder.position.y = 160;
+	cylinder.position.z = 0;
+	scene.add( cylinder );		
+
+	cylinder = new THREE.Mesh( 
+		new THREE.CylinderGeometry( 12/2, 12/2, 390 - 100, 32 ), bodyMaterial );
+	cylinder.position.x = 0;
+	cylinder.position.y = 160 + 390/2 - 100;
+	cylinder.position.z = 0;
+	scene.add( cylinder );
+	
+	// glass stem
+	sphere = new THREE.Mesh(
+		new THREE.SphereGeometry( 116/2, 32, 16 ), glassMaterial );
 	sphere.position.x = 0;
 	sphere.position.y = 160;
 	sphere.position.z = 0;
 	scene.add( sphere );
 
-	cylinder = new THREE.Mesh(
-		new THREE.CylinderGeometry( 24/2, 24/2, 390, 32 ), bodyMaterial );
+	cylinder = new THREE.Mesh( 
+		new THREE.CylinderGeometry( 24/2, 24/2, 390, 32 ), glassMaterial );
 	cylinder.position.x = 0;
 	cylinder.position.y = 160 + 390/2;
 	cylinder.position.z = 0;
 	scene.add( cylinder );
-
+				
 	// head
 	sphere = new THREE.Mesh(
 		new THREE.SphereGeometry( 104/2, 32, 16 ), headMaterial );
@@ -147,14 +169,14 @@ function createDrinkingBird() {
 	scene.add( sphere );
 
 	// hat
-	cylinder = new THREE.Mesh(
+	cylinder = new THREE.Mesh( 
 		new THREE.CylinderGeometry( 142/2, 142/2, 10, 32 ), hatMaterial );
 	cylinder.position.x = 0;
 	cylinder.position.y = 160 + 390 + 40 + 10/2;
 	cylinder.position.z = 0;
 	scene.add( cylinder );
-
-	cylinder = new THREE.Mesh(
+				
+	cylinder = new THREE.Mesh( 
 		new THREE.CylinderGeometry( 80/2, 80/2, 70, 32 ), hatMaterial );
 	cylinder.position.x = 0;
 	cylinder.position.y = 160 + 390 + 40 + 10 + 70/2;
@@ -165,9 +187,6 @@ function createDrinkingBird() {
 function init() {
 	var canvasWidth = 846;
 	var canvasHeight = 494;
-	// For grading the window is fixed in size; here's general code:
-	//var canvasWidth = window.innerWidth;
-	//var canvasHeight = window.innerHeight;
 	var canvasRatio = canvasWidth / canvasHeight;
 
 	// RENDERER
@@ -181,10 +200,8 @@ function init() {
 	camera = new THREE.PerspectiveCamera( 45, canvasRatio, 1, 4000 );
 	// CONTROLS
 	cameraControls = new THREE.OrbitControls(camera, renderer.domElement);
-	camera.position.set( -480, 659, -619 );
+	camera.position.set( -417, 367, -234);
 	cameraControls.target.set(4,301,92);
-
-	fillScene();
 }
 
 function addToDOM() {
@@ -210,12 +227,11 @@ function drawGrid(size, divisions){
 }
 
 try {
-	init();
-	fillScene();
+  init();
+  fillScene();
 	addToDOM();
-	drawGrid(10,10);
-	animate();
+  animate();
 } catch(e) {
-	var errorReport = "Your program encountered an unrecoverable error, can not draw on canvas. Error was:<br/><br/>";
-	console.log(errorReport+e);
+    var errorReport = "Your program encountered an unrecoverable error, can not draw on canvas. Error was:<br/><br/>";
+		console.log(errorReport+e);
 }
